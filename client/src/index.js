d66b3420a9cd88249b73e7a3806bc6a310fd4b3e
Taher Abouzeid <tabouzeid@gmail.com>
2025-01-08 18:32:58 -0500

    working application

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
